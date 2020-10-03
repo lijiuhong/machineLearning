ff9aa9521696f86caae4746e7ce2f6117a1bdc28
lijiuhong <397363163@qq.com>
2020-10-03 09:40:45 +0800

    初始化

---
+++ code/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :hello.py
+# @Time      :2020/10/1 11:19
+# @Author    :Lijiuhong
+
+# import numpy as np
+#
+# print(np.random.rand(3,3))
+
+print("hello word")
