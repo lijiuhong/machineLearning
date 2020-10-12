1bef406ef9a1eeabd8e74d1ab1d2e5cbb4f1ba08
lijiuhong <397363163@qq.com>
2020-10-12 22:50:02 +0800

    再次提交
    Signed-off-by: lijiuhong <397363163@qq.com>

--- code/hello.py
+++ code/hello.py
@@ -4,8 +4,8 @@
 # @Time      :2020/10/1 11:19
 # @Author    :Lijiuhong
 
-# import numpy as np
-#
-# print(np.random.rand(3,3))
+ import numpy as np
+
+ print(np.random.rand(3,3))
 
 print("hello word")
